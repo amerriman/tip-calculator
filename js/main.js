f0a08153d67d8fef4ee2a7b3649dd61ccd5a4966
amerriman <amm434@gmail.com>
2015-07-29 22:57:58 -0600

    completed caluclator functionality

--- js/main.js
+++ js/main.js
@@ -3,16 +3,27 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 
-  $('#calculate').on("click", function(event){
+  // $('#calculate').on("click", function(event){
+  //   event.preventDefault();
+  //   //take the bill total
+  //   var bill = $('#bill').val().replace("$", "");
+  //   var tipAmount = (bill * 20 / 100).toFixed(2);
+  //   $('#showAmount').html("You should tip " + "$"+tipAmount);
+
+  // });
+
+$('#calculate').on("click", function(event){
     event.preventDefault();
     //take the bill total
     var bill = $('#bill').val().replace("$", "");
-    var tipAmount = (bill * 20 / 100).toFixed(2);
+    var tipPercent = $('#tipAmount').val();
+    if(tipPercent === null){
+      tipPercent = 18;
+    }
+    var tipAmount = (bill * tipPercent / 100).toFixed(2);
     $('#showAmount').html("You should tip " + "$"+tipAmount);
 
   });
-
-
 
 
 });
