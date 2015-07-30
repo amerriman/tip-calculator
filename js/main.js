b15d2d5a237330f920ac6df12ff1ccdac9ef661a
amerriman <amm434@gmail.com>
2015-07-29 21:06:58 -0600

    finished basic functionality

--- js/main.js
+++ js/main.js
@@ -2,4 +2,17 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  $('#calculate').on("click", function(event){
+    event.preventDefault();
+    //take the bill total
+    var bill = $('#bill').val();
+    var tipAmount = (bill * 20 / 100).toFixed(2);
+    $('#showAmount').html("You should tip " + "$"+tipAmount);
+
+  });
+
+
+
+
 });
