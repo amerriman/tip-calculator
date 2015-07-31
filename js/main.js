139121858f6f94e4ff9e655a191995200943f3b5
amerriman <amm434@gmail.com>
2015-07-31 13:40:19 -0600

    functional but not refactored. Yet.

--- js/main.js
+++ js/main.js
@@ -12,18 +12,27 @@ $(document).on('ready', function() {
 
   // });
 
-$('#calculate').on("click", function(event){
+  // function calculator(price, amount){
+
+  // }
+
+
+
+  $('#calculate').on("click", function(event){
     event.preventDefault();
-    //take the bill total
+    //take the value of the bill total and replace the money sign with nothing
     var bill = $('#bill').val().replace("$", "");
+    //hold the value of the tip percent they chose
     var tipPercent = $('#tipAmount').val();
+    //if they didn't choose a percent, it will automatically be 18
     if(tipPercent === null){
       tipPercent = 18;
     }
+    //the amount they should tip will be caluclated with the cost of the bill, times the percent chosen, divided by 100 and it will always show 2 characters after the decimal point
     var tipAmount = (bill * tipPercent / 100).toFixed(2);
+
+    //appends the tip amount to the screen
     $('#showAmount').html("You should tip " + "$"+tipAmount);
-
   });
-
 
 });
